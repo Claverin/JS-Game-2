2b59e7914137fbe666f2cba69a0af0ea2e65ebfb
Piotr Michalski <PiotrMichalski@github.com>
2019-03-12 20:55:10 +0100

    Update memory.js

--- memory.js
+++ memory.js
@@ -1,37 +1,14 @@
 var cards = ["ciri.png","geralt.png","jaskier.png","jaskier.png","iorweth.png","triss.png","geralt.png","yen.png","ciri.png","triss.png","yen.png","iorweth.png"];
+shuffle(cards);
 //alert (cards[4]);
 
 //console.log(cards);
-
-var c0 = document.getElementById("c0");
-var c1 = document.getElementById("c1");
-var c2 = document.getElementById("c2");
-var c3 = document.getElementById("c3");
-
-var c4 = document.getElementById("c4");
-var c5 = document.getElementById("c5");
-var c6 = document.getElementById("c6");
-var c7 = document.getElementById("c7");
-
-var c8 = document.getElementById("c8");
-var c9 = document.getElementById("c9");
-var c10 = document.getElementById("c10");
-var c11 = document.getElementById("c11");
-
-c0.addEventListener("click",function() {revealCard(0); });
-c1.addEventListener("click",function() {revealCard(1); });
-c2.addEventListener("click",function() {revealCard(2); });
-c3.addEventListener("click",function() {revealCard(3); });
-
-c4.addEventListener("click",function() {revealCard(4); });
-c5.addEventListener("click",function() {revealCard(5); });
-c6.addEventListener("click",function() {revealCard(6); });
-c7.addEventListener("click",function() {revealCard(7); });
-
-c8.addEventListener("click",function() {revealCard(8); });
-c9.addEventListener("click",function() {revealCard(9); });
-c10.addEventListener("click",function() {revealCard(10); });
-c11.addEventListener("click",function() {revealCard(11); });
+var c = [];
+for (let i = 0; i < 12; i++) 
+{
+    c[i] = document.getElementById('c' + i);
+    c[i].addEventListener("click", () => revealCard(i));
+}
 
 var oneVisible = false;
 var turnCounter = 0;
@@ -45,7 +22,7 @@ function revealCard(nr){
 
     //alert('Opacity: '+opacityValue)
 
-    if(opacityValue != 0 && lock == false){
+    if(opacityValue != 0 && lock == false  && visible_nr != nr){
         lock = true;
 
         //alert (nr);
@@ -65,7 +42,7 @@ function revealCard(nr){
         else{
             //secound card
 
-            if(cards[visible_nr] == cards[nr] && visible_nr != nr){
+            if(cards[visible_nr] == cards[nr]){
                 //alert("para!");
                 setTimeout(function() {hide2Cards(nr, visible_nr)}, 750);
             }
@@ -104,4 +81,14 @@ function restore2Cards(nr1, nr2){
     $('#c'+nr2).removeClass('cardA');
 
     lock = false;
+}
+
+function shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
 }
